81d04635880127bc5b7dbdb5355891afd4aaac42
Gabriel Muñoz <gabriel.munoz@assurant.com>
2015-12-01 22:42:24 -0600

    Add chapter 1.7 content

--- 1.4.scm
+++ 1.4.scm
@@ -123,3 +123,39 @@
 
 (define (s.update! id env value)
   (putprop id 'apval value) )
+
+; 1.7 - Global environment
+(define env.global env.init)
+(define-syntax definitial
+  (syntax-rules ()
+	((definitial name)
+	 (begin (set! env.global (cons (cons 'name 'void) env.global))
+			'name ) )
+	((definitial name value)
+	 (begin (set! env.global (cons (cons 'name value) env.global))
+			'name ) ) ) )
+(define-syntax defprimitive
+  (syntax-rules ()
+	((defprimitive name value arity)
+	 (definitial name
+		(lambda (values)
+		  (if (= arity (length values))
+			(apply value values)         ; The real apply of Scheme
+			(wrong "Incorrect arity"
+				   (list 'name values) ) ) ) ) ) ) )
+
+(definitial t #t)
+(definitial f the-false-value)
+(definitial nil '())
+
+(definitial foo)
+(definitial bar)
+(definitial fib)
+(definitial fact)
+
+(defprimitive cons cons 2)
+(defprimitive car car 1)
+(defprimitive set-cdr! set-cdr! 2)
+(defprimitive + + 2)
+(defprimitive eq? eq? 2)
+(defprimitive < < 2)
