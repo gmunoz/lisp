5d8a728e6fe1375054f28d6d4acacdef59565e3a
Gabriel Muñoz <gabriel@gmz.io>
2015-12-08 12:47:59 -0500

    Add ch1 evaluator, which is lisp1

--- 2.scm
+++ 2.scm
@@ -119,6 +119,49 @@
 	    (invoke (car args) (cdr args))
 		(wrong "Incorrect arity" 'funcall) ) ) )
 
+; Lisp1 from chapter1
+
+(define (evaluate e env)
+  (if (atom? e)
+	(cond ((symbol? e) (lookup e env))
+		  ((or (number? e)(string? e)(char? e)(boolean? e)(vector? e))
+		   e )
+		  (else (wrong "Cannot evaluate" e)) )
+	(case (car e)
+	  ((quote)  (cadr e))
+	  ((if)     (if (evaluate (cadr e) env)
+				    (evaluate (caddr e) env)
+					(evaluate (cadddr e) env) ))
+	  ((begin)  (eprogn (cdr e) env))
+	  ((set!)   (update! (cadr e) env (evaluate (caddr e) env)))
+	  ((lambda) (make-function (cadr e) (cddr e) env))
+	  (else     (invoke (evaluate (car e) env)
+						(evlis (cdr e) env) )) ) ) )
+
+(define (eprogn exps env)
+  (if (pair? exps)
+	(if (pair? (cdr exps))
+	  (begin (evaluate (car exps) env)
+			 (eprogn (cdr exps) env) )
+	  (evaluate (car exps) env) )
+	empty-begin ) )
+
+(define (evlis exps env)
+  (if (pair? exps)
+	  (cons (evaluate (car exps) env)
+			(evlis (cdr exps) env) )
+	  '() ) )
+
+(define (make-function variables body env)
+  (lambda (values)
+	(eprogn body (extend env variables values)) ) )
+
+(define (chapter2-lisp1)
+  (define (toplevel)
+	(display (evaluate (read) env.global))
+	(toplevel) )
+  (toplevel) )
+
 ; 2.5.1 Dynamic variables
 
 (define (df.evaluate e env fenv denv)
